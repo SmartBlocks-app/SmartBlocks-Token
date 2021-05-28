a94b52ade135256deeee3c9bf093fa2b5caf361e
Ash Simpson <ash.simpson@gmail.com>
2021-05-28 12:11:27 +1000

    Update SmartBlocks.sol

--- SmartBlocks.sol
+++ SmartBlocks.sol
@@ -14,7 +14,7 @@
 interface IERC20Minimal {    
 	function balanceOf(address account) external view returns (uint256);
 	function transfer(address recipient, uint256 amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
+        function allowance(address owner, address spender) external view returns (uint256);
 	function approve(address spender, uint256 amount) external returns (bool);
 	function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 	event Transfer(address indexed from, address indexed to, uint256 value);
